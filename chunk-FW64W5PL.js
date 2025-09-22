60095b581e156bb6b3834738d472f17f53c28182
Nahin Munkar <nahinmunkarnm@gmail.com>
2025-09-22 17:57:52 +0600

    Auto-generated commit

---
+++ chunk-FW64W5PL.js
@@ -0,0 +1,1 @@
+import{Pa as a,Qa as n,Wa as i,wa as o}from"./chunk-6D6YVWDB.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(a(0,"p"),i(1,"about works!"),n())},encapsulation:2})};export{s as About};
